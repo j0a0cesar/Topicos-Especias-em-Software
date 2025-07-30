7ee71b568129b56a3dc8ba58cebdfb4ad11afd24
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-07-30 10:39:04 -0300

    Create coversao de metros para dm cm mm.py

---
+++ aula 1/coversao de metros para dm cm mm.py
@@ -0,0 +1,12 @@
+metros = int(input("Digite a quantidade em metros: "))
+
+
+
+dec = float(metros * 10)
+print(f"É equivalente a: {dec}dm")
+
+cem = float(metros * 100)
+print(f"É equivalente a: {cem}cm")
+
+mili = float(metros * 1000)
+print(f"É equivalente a: {mili}mm")
