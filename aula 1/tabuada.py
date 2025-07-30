7793880dba5272e003bdc1e4f7711f24b2c0288b
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-07-30 10:51:20 -0300

    Create tabuada.py

---
+++ aula 1/tabuada.py
@@ -0,0 +1,42 @@
+print("Tabuada:")
+
+i = 0
+
+for i  in range(10):
+    print(f"1 x {i} =",1*i)
+print("\n")
+
+for i  in range(10):
+    print(f"2 x {i} =",2*i)
+
+for i  in range(10):
+    print(f"3 x {i} =",3*i)
+print("\n")
+
+for i  in range(10):
+    print(f"4 x {i} =",4*i)
+print("\n")
+
+for i  in range(10):
+    print(f"5 x {i} =",5*i)
+print("\n")
+
+for i  in range(10):
+    print(f"6 x {i} =",6*i)
+print("\n")
+
+for i  in range(10):
+    print(f"7 x {i} =",7*i)
+print("\n")
+
+for i  in range(10):
+    print(f"8 x {i} =",8*i)
+print("\n")
+
+for i  in range(10):
+    print(f"9 x {i} =",9*i)
+print("\n")
+
+for i  in range(10):
+    print(f"10 x {i} =",10*i)
+print("\n")
