e1010686614c35f6824e8193165776277464758d
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-08-26 17:45:50 -0300

    Update halfling.py

--- Old Dragon/raca/halfling.py
+++ Old Dragon/raca/halfling.py
@@ -4,5 +4,13 @@ class Halfling(Raca):
         super().__init__()
 
         self.alinhamento = "Neutro"
-        self.infravisao = "Não"
+        self.infravisao = "Não possui"
         self.movimento = 6
+        self.habilidades_raca = [
+            "Furtivos: Bônus para se esconder.",
+            "Destemidos: Bônus de +1 em qualquer teste de JPS (Jogada de Proteção de Sabedoria).",
+            "Bons de Mira: Ataques fáceis com armas de arremesso.",
+            "Pequenos: Ataques de criaturas grandes são difíceis para acertá-los.",
+            "Restrições: Apenas armaduras de couro e armas pequenas ou médias."
+        ]
+        self.peso_base = 35.0
