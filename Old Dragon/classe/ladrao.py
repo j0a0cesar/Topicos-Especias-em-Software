8285130511072c3ed1f6b4b0a428b78315515d89
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-08-26 17:39:28 -0300

    Update ladrao.py

--- Old Dragon/classe/ladrao.py
+++ Old Dragon/classe/ladrao.py
@@ -5,7 +5,10 @@ from classe import Classe
 
 class Ladrao(Classe):
     def __init__(self):
-        super().__init__()
-        self.armadura = "pode usar apenas as armaduras leves"
-        self.armas = "pode usar apenas as armas pequenas ou médias"
-        self.intensMagigos = "não pode usar cajados, não pode varinhas e pergaminhos com exceçao dos pergaminhos de proteção"
+        super().__init__(vida=6, ataque=1, protecao=5)
+        self.armadura = "Apenas armaduras leves"
+        self.armas = "Apenas armas pequenas ou médias"
+        self.itens_magicos = "Não pode usar cajados, varinhas e pergaminhos (exceto os de proteção)"
+        self.habilidades_classe = {
+            1: ["Ataque Furtivo", "Ouvir Ruídos", "Talentos de Ladrão"]
+        }
