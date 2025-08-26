5fb154e3d1a0059fc8cb86a4a83cb371e4c75f84
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-08-26 17:15:30 -0300

    Create classe.py

---
+++ Old Dragon/classe/classe.py
@@ -0,0 +1,10 @@
+class Classe:
+    def __init__(self,vida,ataque,protecao):
+        self.armadura = ""
+        self.armas = ""
+        self.ataque = ataque
+        self.habilidades_classe = []
+        self.intensMagigos = ""
+        self.nivel = 1
+        self.protecao = protecao
+        self.vida = vida
