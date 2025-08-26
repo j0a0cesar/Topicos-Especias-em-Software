b5645a6b74566bee5ce1528cfe2025632017e76a
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-08-26 17:16:02 -0300

    Create clerigo.py

---
+++ Old Dragon/classe/clerigo.py
@@ -0,0 +1,8 @@
+from classe import Classe
+
+class Clerigo(Classe):
+    def __init__(self):
+        super().__init__()
+        self.armadura = "pode usar todas as armaduras"
+        self.armas = "só podem usar armas impactantes"
+        self.intensMagigos = "o Clérigo é capaz de usar todos os tipos de itens mágicos desde que sejam ordeiros"
