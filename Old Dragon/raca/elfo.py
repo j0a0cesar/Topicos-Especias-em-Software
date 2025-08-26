9138fb213fe714a05b099917175fdc29885d53e4
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-08-26 17:14:42 -0300

    Create elfo.py

---
+++ Old Dragon/raca/elfo.py
@@ -0,0 +1,7 @@
+from raca import Raca
+class Elfo:
+    def __init__(self):
+        super().__init__()
+        self.alinhamento = "Neutro"
+        self.infravisao = "18 metros"
+        self.movimento = 9
