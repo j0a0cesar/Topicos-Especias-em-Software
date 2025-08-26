c6b2f9014d356068fef3420a50f077954de05b9d
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-08-26 17:12:57 -0300

    Create raca.py

---
+++ Old Dragon/raca/raca.py
@@ -0,0 +1,8 @@
+class Raca:
+
+    def __init__(self):
+        self.alinhamento = ""
+        self.habilidades_raca = []
+        self.infravisao = ""
+        self.movimento = 0
+        self.peso_base = 0.0
