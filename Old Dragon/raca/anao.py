bba8e26b7b703456015c57b72cea1b2bd100ed0d
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-08-26 17:41:52 -0300

    Update anao.py

--- Old Dragon/raca/anao.py
+++ Old Dragon/raca/anao.py
@@ -5,3 +5,10 @@ class Anao(Raca):
         self.alinhamento = "Ordem"
         self.infravisao = "18 metros"
         self.movimento = 6
+        self.habilidades_raca = [
+            "Mineradores: Habilidade de detectar anomalias em pedras.",
+            "Vigoroso: Bônus de +1 em qualquer teste de JPC (Jogada de Proteção de Constituição).",
+            "Restrição de Armas: Não podem usar armas grandes.",
+            "Inimigos Naturais: Ataques fáceis contra orcs, ogros e hobgoblins."
+        ]
+        self.peso_base = 75.0
