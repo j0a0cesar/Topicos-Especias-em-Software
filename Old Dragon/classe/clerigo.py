c8ad0f0d1344159520c878c2e704821975e5b010
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-08-26 17:40:23 -0300

    Update clerigo.py

--- Old Dragon/classe/clerigo.py
+++ Old Dragon/classe/clerigo.py
@@ -2,7 +2,10 @@ from classe import Classe
 
 class Clerigo(Classe):
     def __init__(self):
-        super().__init__()
-        self.armadura = "pode usar todas as armaduras"
-        self.armas = "só podem usar armas impactantes"
-        self.intensMagigos = "o Clérigo é capaz de usar todos os tipos de itens mágicos desde que sejam ordeiros"
+        super().__init__(vida=8, ataque=1, protecao=5)
+        self.armadura = "Pode usar todas as armaduras"
+        self.armas = "Apenas armas de impacto"
+        self.itens_magicos = "Pode usar todos os itens mágicos ordeiros"
+        self.habilidades_classe = {
+            1: ["Magias Divinas", "Afastar Mortos-Vivos", "Cura Milagrosa"]
+        }
