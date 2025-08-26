e3c28e26899092a5e3134b8f2a46efd0a47bc7eb
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-08-26 17:13:21 -0300

    Create meio_elfo.py

---
+++ Old Dragon/raca/meio_elfo.py
@@ -0,0 +1,7 @@
+from raca import Raca
+class MeioElfo(Raca):
+    def __init__(self):
+        super().__init__()
+        self.alinhamento = "Caos"
+        self.infravisao = "9 metros"
+        self.movimento = 9
