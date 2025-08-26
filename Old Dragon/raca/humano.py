bc3f37733cbbafc749c1afedbfa0977fb83595e4
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-08-26 17:45:12 -0300

    Update humano.py

--- Old Dragon/raca/humano.py
+++ Old Dragon/raca/humano.py
@@ -3,7 +3,11 @@ from raca import Raca
 class Humano(Raca):
     def __init__(self):
         super().__init__()
-
         self.alinhamento = "Qualquer"
-        self.infravisao = "Não"
+        self.infravisao = "Não possui"
         self.movimento = 9
+        self.habilidades_raca = [
+            "Aprendizado: Bônus de 10% sobre toda experiência (XP) recebida.",
+            "Adaptabilidade: Bônus de +1 em uma única Jogada de Proteção à sua escolha."
+        ]
+        self.peso_base = 80.0
