780153f0abe2453729c0a7d16e8c36b274764ea5
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-08-26 17:15:06 -0300

    Create anao.py

---
+++ Old Dragon/raca/anao.py
@@ -0,0 +1,7 @@
+from raca import Raca
+class Anao(Raca):
+    def __init__(self):
+        super().__init__()
+        self.alinhamento = "Ordem"
+        self.infravisao = "18 metros"
+        self.movimento = 6
