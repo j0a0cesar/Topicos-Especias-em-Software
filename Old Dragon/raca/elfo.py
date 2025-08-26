ebbe5caf9d959abaafb1ff7a0284df68b23b828d
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-08-26 17:43:13 -0300

    Update elfo.py

--- Old Dragon/raca/elfo.py
+++ Old Dragon/raca/elfo.py
@@ -2,6 +2,13 @@ from raca import Raca
 class Elfo:
     def __init__(self):
         super().__init__()
-        self.alinhamento = "Neutro"
+         self.alinhamento = "Neutro"
         self.infravisao = "18 metros"
         self.movimento = 9
+        self.habilidades_raca = [
+            "Percepção Natural: Chance de detectar portas secretas.",
+            "Graciosos: Bônus de +1 em qualquer teste de JPD (Jogada de Proteção de Destreza).",
+            "Arma Racial: Bônus de +1 nos danos com arcos.",
+            "Imunidades: Imune a sono e paralisia de Ghoul."
+        ]
+        self.peso_base = 60.0
